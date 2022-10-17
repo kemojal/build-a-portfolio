3c9f528fdbfa7149629641424b32a31ac7088ea1
kemo <kemo3855@yahoo.com>
2022-10-18 07:35:31 +0800

    skill section

--- components/sections/skills/index.js
+++ components/sections/skills/index.js
@@ -20,12 +20,12 @@ export default function Skills() {
     'User Interviews',
   ];
   return (
-    <section className='w-full max-w-[944px] flex items-center justify-center border-gray-300 border-b-2 pb-12 pt-12'>
+    <section className='w-full max-w-[944px] flex items-center justify-center border-gray-100 dark:border-gray-900 border-b-2 pb-12 pt-12'>
       <div className='px-4 flex-col items-center justify-center  w-full'>
         <Title title='Skills' />
-        <div className='pt-4  w-full flex flex-wrap'>
+        <div className='pt-4  w-full flex items-center justify-center flex-wrap'>
           {exp.map((item, i) => (
-            <div className='px-4 py-1 m-1 my-1 border-gray-300 border-2 rounded-md bg-gray-100'>
+            <div className='px-4 py-1 m-2 my-2 border-gray-100 dark:border-gray-900 border-2 rounded-full bg-gray-100 dark:bg-gray-800 sm: m-1'>
               <h1 className='text-gray-500'>{item}</h1>
             </div>
           ))}
