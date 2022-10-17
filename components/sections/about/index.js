e8b91a3fe9f31741f66ae63fa092786a94590226
kemo <kemo3855@yahoo.com>
2022-10-18 07:31:28 +0800

    about index section

--- components/sections/about/index.js
+++ components/sections/about/index.js
@@ -2,10 +2,10 @@ import Title from '../../shared/Title';
 
 export default function About() {
   return (
-    <section className='max-w-[944px] flex items-center justify-center border-gray-300 border-b-2 pb-12 pt-12'>
+    <section className='max-w-[944px] flex items-center justify-center border-gray-100 dark:border-gray-900 border-b-2 pb-12 pt-12'>
       <div className='px-4 flex-col items-center justify-center '>
         <Title title='About' />
-        <p className='pt-2 flex-col'>
+        <p className='pt-2 flex-col text-gray-500 text-lg'>
           <span>
             I'm Onur, I live in Istanbul. I am a digital product designer at
             Apple. I’m a multidisciplinary designer with 6+ years of combined
