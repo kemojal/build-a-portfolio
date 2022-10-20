eb08ad06e2789852a1172dae8cc924e0b2474f83
kemo <kemo3855@yahoo.com>
2022-10-20 13:46:36 +0800

    refactor index page

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,38 @@ import Experience from '../components/sections/experience';
 import Skills from '../components/sections/skills';
 import Portfolio from '../components/sections/portfolio';
 import Contact from '../components/sections/contact';
+import { FooterItem } from '../components/FooterItem';
 export default function Home() {
+  const footerData = [
+    {
+      title: 'Dribble',
+      href: '',
+      icon: 'dribble',
+
+    },
+    {
+      title: 'Behance',
+      href: '',
+      icon: 'behance',
+
+    },
+    {
+      title: 'LinkedIn',
+      href: '',
+      icon: 'linkedin',
+
+    },
+    {
+      title: 'Twitter',
+      href: '',
+      icon: 'twitter',
+
+    }
+  ]
   return (
     <div className='container sm:px-0 bg-white dark:bg-black text-stone-900 dark:text-slate-50'>
       <Head>
-        <title>Create Next App</title>
+        <title>Porfolio</title>
       </Head>
 
       <main className={styles.main}>
@@ -25,7 +52,14 @@ export default function Home() {
 
       <footer className='w-full flex items-center justify-center border-b-2 pb-12 pt-0'>
         <div className='max-w-[50%] px-4 flex items-center justify-between  w-full '>
-          <a href='https://next.new' target='_blank' rel='noopener noreferrer'>
+          {
+            footerData.map(
+              (item, i)=>(
+                <FooterItem key={i} href={item.href} title={item.title} />
+              )
+            )
+          }
+          {/* <a href='https://next.new' target='_blank' rel='noopener noreferrer'>
             Dribble
           </a>
           <a href='https://next.new' target='_blank' rel='noopener noreferrer'>
@@ -36,7 +70,7 @@ export default function Home() {
           </a>
           <a href='https://next.new' target='_blank' rel='noopener noreferrer'>
             Twitter
-          </a>
+          </a> */}
         </div>
       </footer>
     </div>
