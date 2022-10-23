4149a74b17777141d3e21c133c1e31bb782e6989
kemo <kemo3855@yahoo.com>
2022-10-23 18:05:48 +0800

    rendericonByname

--- components/sections/experience/renderIconByName.js
+++ components/sections/experience/renderIconByName.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const RenderIconByName = (icon, children) => {
-  return <div>RenderIconByName</div>;
+return <div>RenderIconByName</div>;
 };
 
 export default RenderIconByName;
