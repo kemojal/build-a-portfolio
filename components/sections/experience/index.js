68ea7c2b86b85be235b5182a27f9c3ad65f2713b
kemo <kemo3855@yahoo.com>
2022-10-25 23:51:07 +0800

    experience

--- components/sections/experience/index.js
+++ components/sections/experience/index.js
@@ -1,5 +1,6 @@
 import Title from '../../shared/Title';
-import { Apple } from 'iconsax-react';
+import { Apple, Dropbox } from 'iconsax-react';
+
 export default function Experience() {
   const exp = [
     {
@@ -31,12 +32,20 @@ export default function Experience() {
           {exp.map((item, i) => (
             <div className='flex w-1/2 '>
               <div className=' w-16 h-16 rounded-full bg-gray-50 flex items-center justify-center'>
-                <Apple size='38' color='#BF125D' variant='Bold' />
+                {item.company === 'Apple' && (
+                  <Apple size='38' color='#BF125D' variant='Bold' />
+                )}
+                {/* {item.company === 'Airbnb' && (
+                  <Airbnb size='38' color='#BF125D' variant='Bold' />
+                )} */}
+                {item.company === 'Dropbox' && (
+                  <Dropbox size='38' color='#BF125D' variant='Bold' />
+                )}
               </div>
               <div className='pl-8 pb-8 pt-4'>
                 <h1 className='font-bold'>{item.company}</h1>
                 <h3 className='text-gray-500'>
-                  {item.position + '. ' + item.date}
+                  {item.position + '. ' + item.date}coocoococo
                 </h3>
               </div>
             </div>
